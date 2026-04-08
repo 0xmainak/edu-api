9d90612576536469e90e378bbf98a5c149077434
0xmainak <mainakpanda30@gmail.com>
2026-04-09 04:00:02 +0530

    feat: add root endpoint with API metadata to app configuration

--- src/app.js
+++ src/app.js
@@ -7,6 +7,15 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "hello world",
+    name: "edu-api",
+    version: "1.0.0",
+    description: "School Management API",
+  });
+});
+
 app.use("/", healthRoutes);
 app.use("/", schoolRoutes);
 
